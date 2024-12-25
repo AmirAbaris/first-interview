f16601fb79ff3c9175caa6f3d862bfd5e4ac9856
Amir Abaris <thisisamirabaris@gmail.com>
2024-12-25 16:54:23 +0330

    fixed and added a safe guard to access local storage in global state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
   user: { email: string } | null;
@@ -12,13 +12,28 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [user, setUser] = useState<{ email: string } | null>(() => {
-    const storedUser = localStorage.getItem("user");
-    return storedUser ? JSON.parse(storedUser) : null;
-  });
+  const [user, setUser] = useState<{ email: string } | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && localStorage.getItem("user")) {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    }
+  }, []);
+
+  const safeSetUser = (newUser: { email: string } | null) => {
+    setUser(newUser);
+    if (typeof window !== "undefined" && newUser) {
+      localStorage.setItem("user", JSON.stringify(newUser));
+    } else if (typeof window !== "undefined") {
+      localStorage.removeItem("user");
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser: safeSetUser }}>
       {children}
     </AuthContext.Provider>
   );
